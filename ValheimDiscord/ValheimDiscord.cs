993562836473eddae9d95dfdb1895a893a499009
Chester J. Ayala <chesterayala.ca@gmail.com>
2025-02-28 09:34:40 +0800

    Add plugin entrypoint

---
+++ ValheimDiscord/ValheimDiscord.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace ValheimDiscord
+{
+    [BepInPlugin(_pluginGuid, _pluginName, _pluginVersion)]
+    public sealed class ValheimDiscord : BaseUnityPlugin
+    {
+        private const string _pluginGuid = "com.keirloire.valheimdiscord";
+        private const string _pluginName = "ValheimDiscord";
+        private const string _pluginVersion = "1.0.0";
+
+        public void Log(string message) => Debug.Log($"[{_pluginName}] {message}");
+
+        public void Awake()
+        {
+            this.Log("Loaded.");
+        }
+
+    }
+}
