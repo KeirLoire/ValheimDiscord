6d7141bf0b178596400f28bc1f79bf2f9a540174
Chester J. Ayala <chesterayala.ca@gmail.com>
2025-02-28 10:44:59 +0800

    Add server join and leave messages to Discord

---
+++ ValheimDiscord/Patches/ZNetPatch.cs
@@ -0,0 +1,68 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace ValheimDiscord.Patches
+{
+    public sealed class ZNetPatch
+    {
+        [HarmonyPatch]
+        public static class RPC_PeerInfo
+        {
+            public static MethodBase TargetMethod()
+            {
+                var type = AccessTools.TypeByName("ZNet");
+
+                return AccessTools.Method(type, "RPC_PeerInfo");
+            }
+
+            public static void Postfix(ZNet __instance, ZRpc rpc, ZPackage pkg)
+            {
+                var type = __instance.GetType();
+                var method = type.GetMethod("GetPeer", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                if (method == null)
+                    return;
+
+                var peer = method.Invoke(__instance, new object[] { rpc }) as ZNetPeer;
+
+                if (peer == null)
+                    return;
+
+                if (string.IsNullOrWhiteSpace(peer.m_playerName))
+                    return;
+
+                ValheimDiscord.SendDiscordChat($"{peer.m_playerName} has joined the server.");
+            }
+        }
+
+        [HarmonyPatch]
+        public static class Disconnect
+        {
+            public static MethodBase TargetMethod()
+            {
+                var type = AccessTools.TypeByName("ZNet");
+
+                return AccessTools.Method(type, "RPC_Disconnect");
+            }
+
+            public static void Prefix(ZNet __instance, ZRpc rpc)
+            {
+                var type = __instance.GetType();
+                var method = type.GetMethod("GetPeer", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                if (method == null)
+                    return;
+
+                var peer = method.Invoke(__instance, new object[] { rpc }) as ZNetPeer;
+
+                if (peer == null)
+                    return;
+
+                if (string.IsNullOrWhiteSpace(peer.m_playerName))
+                    return;
+
+                ValheimDiscord.SendDiscordChat($"{peer.m_playerName} has left the server.");
+            }
+        }
+    }
+}
